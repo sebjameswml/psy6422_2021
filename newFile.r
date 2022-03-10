123e5dbffeb9e0bb63c2d91d43f2a5193afd3e7b
Seb James <seb.james@sheffield.ac.uk>
2022-03-10 11:34:25 +0000

    Plot a graph of x squared

--- newFile.r
+++ newFile.r
@@ -1,1 +1,10 @@
 # This is some other code file.
+
+# A sequence of numbers for the x axis
+b<-seq(-10, 10, length=50)
+# b squared
+c<-b^2
+# Plot c vs b
+plot (b, c)
+# Find the linear model for c predicted by b (fit will be *bad*)
+abline(lm(c~b))
